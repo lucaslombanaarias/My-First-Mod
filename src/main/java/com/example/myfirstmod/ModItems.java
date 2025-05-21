e10a9c8ae390a27aa87cbd91df57c269ee9b10e7
lucaslombanaarias <lucaslombanaarias@gmail.com>
2025-05-20 23:52:56 -0500

    Populate mods.toml with actual mod id, version, name, authors, etc.

---
+++ src/main/java/com/example/myfirstmod/ModItems.java
@@ -0,0 +1,23 @@
+package com.example.MyFirstMod;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+
+public class ModItems {
+    // Create a DeferredRegister for ITEMS
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MODID);
+
+    // Register the Calmleaf Seed
+    public static final RegistryObject<Item> CALMLEAF_SEED = ITEMS.register("calmleaf_seed",
+            () -> new Item(new Item.Properties()
+                    .setId(ITEMS.key("calmleaf_seed"))   // returns a ResourceLocation under the hood
+                    .stacksTo(64)
+            )
+    );
+
+
+}
--- src/main/java/com/example/myfirstmod/MyFirstMod.java
+++ src/main/java/com/example/myfirstmod/MyFirstMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.example.MyFirstMod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -29,10 +29,10 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod {
+@Mod(MyFirstMod.MODID)
+public class MyFirstMod {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "myfirstmod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -75,7 +75,7 @@ public class ExampleMod {
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod(FMLJavaModLoadingContext context) {
+    public MyFirstMod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
         // Register the commonSetup method for modloading
